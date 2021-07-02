a4490a790ccf96b2bd049d00c403cd41da7ed5d9
fifsky <fifsky@gmail.com>
2021-07-02 18:42:28 +0800

    feat: add test case

--- api/handler/cate_test.go
+++ api/handler/cate_test.go
@@ -81,52 +81,33 @@ func TestCate_Post(t *testing.T) {
 	dbunit.New(t, func(d *dbunit.DBUnit) {
 		db := d.NewDatabase(testutil.Schema(), testutil.Fixtures("cates")...)
 		handler := NewCate(db, repo.NewCate(db))
-		tests := []struct {
-			name         string
-			requestBody  gee.H
-			assertType   string
-			responseBody string
-		}{
+
+		tests := []testutil.TestCase{
 			{
-				"success",
-				gee.H{"name": "demo", "domain": "demo", "desc": "demo", "created_at": "2021-06-29 11:55:09", "updated_at": "2021-06-29 11:55:09"},
-				"equal",
-				`{"code":200,"data":{"id":2,"name":"demo","desc":"demo","domain":"demo","created_at":"2021-06-29 11:55:09","updated_at":"2021-06-29 11:55:09"},"msg":"success"}`,
+				Name:         "success",
+				RequestBody:  gee.H{"name": "demo", "domain": "demo", "desc": "demo", "created_at": "2021-06-29 11:55:09", "updated_at": "2021-06-29 11:55:09"},
+				ResponseBody: `{"code":200,"data":{"id":2,"name":"demo","desc":"demo","domain":"demo","created_at":"2021-06-29 11:55:09","updated_at":"2021-06-29 11:55:09"},"msg":"success"}`,
 			},
 			{
-				"update success",
-				gee.H{"id": 1, "domain": "test2", "name": "test2", "desc": "test2", "updated_at": "2021-06-29 11:55:09"},
-				"contains",
-				`"name":"test2"`,
+				Name:         "update success",
+				RequestBody:  gee.H{"id": 1, "domain": "test2", "name": "test2", "desc": "test2", "updated_at": "2021-06-29 11:55:09"},
+				AssertType:   testutil.AssertContains,
+				ResponseBody: `"name":"test2"`,
 			},
 			{
-				"update error",
-				gee.H{"id": 1, "domain": "demo", "name": "demo", "desc": "demo"},
-				"equal",
-				`{"code":201,"msg":"更新失败"}`,
+				Name:         "update error",
+				RequestBody:  gee.H{"id": 1, "domain": "demo", "name": "demo", "desc": "demo"},
+				ResponseBody: `{"code":201,"msg":"更新失败"}`,
 			},
 			{
-				"params error",
-				gee.H{"name": "demo", "desc": "demo"},
-				"equal",
-				`{"code":201,"msg":"参数错误:缺少domain"}`,
+				Name:         "params error",
+				RequestBody:  gee.H{"name": "demo", "desc": "demo"},
+				ResponseBody: `{"code":201,"msg":"参数错误:缺少domain"}`,
 			},
 		}
 
 		for _, tt := range tests {
-			t.Run(tt.name, func(t *testing.T) {
-				req := test.NewRequest("/api/admin/cate/post", handler.Post)
-				resp, err := req.JSON(tt.requestBody)
-				require.NoError(t, err)
-				require.Equal(t, http.StatusOK, resp.Code)
-
-				switch tt.assertType {
-				case "equal":
-					require.Equal(t, tt.responseBody, resp.GetBodyString())
-				case "contains":
-					require.Contains(t, resp.GetBodyString(), tt.responseBody)
-				}
-			})
+			tt.Run(t, "/api/admin/cate/post", handler.Post)
 		}
 	})
 }
