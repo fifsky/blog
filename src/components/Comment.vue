4858e893115facf2cdc0ab2f6300e8df5ff55b2f
fifsky <fifsky@gmail.com>
2022-10-13 12:23:55 +0800

    feat: artalk useBackendConf

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -9,6 +9,7 @@
     props: ["postId"],
     mounted() {
       new Artalk({
+        useBackendConf: true,
         el:        '#comments',              // 绑定元素的 Selector
         server:    'https://comment-api.fifsky.com',  // 后端地址
         site:      'FIFSKY',           // 你的站点名
