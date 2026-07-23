b4f1b07176ae8defb0fbd289c871a322560063ac
fifsky <fifsky@gmail.com>
2026-07-23 14:08:30 +0800

    refactor(router): 使用 slices.Backward 简化中间件遍历

--- server/router/middleware.go
+++ server/router/middleware.go
@@ -1,14 +1,17 @@
 package router
 
-import "net/http"
+import (
+	"net/http"
+	"slices"
+)
 
 type Middleware = func(next http.Handler) http.Handler
 
 // chain builds a http.Handler composed of an inline middleware stack and endpoint
 // handler in the order they are passed.
 func chain(endpoint http.Handler, middlewares []Middleware) http.Handler {
-	for i := len(middlewares) - 1; i >= 0; i-- {
-		endpoint = middlewares[i](endpoint)
+	for _, middleware := range slices.Backward(middlewares) {
+		endpoint = middleware(endpoint)
 	}
 	return endpoint
 }
